1e216ad6cd7cc72ec96fe35a07f7276e58df02ec
fede8932 <federi.angel@gmail.com>
2022-10-12 14:24:20 -0300

    actualizacion de backend

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,89 +1,25 @@
-const express = require('express');
-const http = require('http');
-const { User } = require('./models');
+const express = require("express");
+// const db = require("./db");
+// const routes = require("./routes");
+const morgan = require("morgan");
+// require('dotenv').config();
 
 const app = express();
 
-const db = require('./db');
+app.use(express.json());
+app.use(morgan("tiny"));
+// app.use("/api", routes);
 
-const bodyParser = require('body-parser');
-const routes = require('./routes');
-const morgan = require('morgan');
-const nodemailer = require('nodemailer');
-const cookieParser = require('cookie-parser');
-const sessions = require('express-session');
-const passport = require('passport');
-const localStrategy = require('passport-local').Strategy;
+SERVER_PORT = process.env.PORT || 3002
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+console.log(SERVER_PORT);
 
-app.use(morgan('tiny'));
+// db.sync({ force: false }).then((data) => {
+//   app.listen(SERVER_PORT, (req, res) => {
+//     console.log("Server Listening on port: " + SERVER_PORT);
+//   });
+// });
 
-app.use(cookieParser());
-
-app.use(
-  sessions({
-    secret: 'sativaANDco',
-    resave: true,
-    saveUninitialized: true,
-    cookie: { _expires: 60000000000000 },
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(
-  new localStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-    },
-
-    function (email, password, done) {
-      User.findOne({ where: { email } })
-        .then(user => {
-          if (!user) {
-            return done(null, false);
-          }
-
-          user.setHash(password, user.salt).then(hash => {
-            if (hash !== user.password) {
-              return done(null, false);
-            }
-
-            return done(null, user);
-          });
-        })
-        .catch(done);
-    }
-  )
-);
-
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
-
-passport.deserializeUser(function (id, done) {
-  User.findByPk(id)
-    .then(user => {
-      done(null, user);
-    })
-    .catch(done);
-});
-
-app.use(function (err, req, res, next) {
-  console.error(err);
-  res.status(500).send(err);
-});
-
-app.use('/api', routes);
-
-const port = 3001;
-
-db.sync({ force: true }).then(() => {
-  app.listen(port, function () {
-    console.log(`Listening on port http://localhost:${port}`);
-  });
+app.listen(SERVER_PORT, (req, res) => {
+  console.log(`Server running in port: ${SERVER_PORT}`);
 });
