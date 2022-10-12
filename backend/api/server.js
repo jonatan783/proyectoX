fd2315a79f56c2027dc24caf7f6b5de185f6ada2
fede8932 <federi.angel@gmail.com>
2022-10-12 18:49:04 -0300

    actualizacion db

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-// const db = require("./db");
+const db = require("./db");
 // const routes = require("./routes");
 const morgan = require("morgan");
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = express();
 
@@ -12,16 +12,10 @@ app.use(express.json());
 app.use(morgan("tiny"));
 // app.use("/api", routes);
 
-SERVER_PORT = process.env.PORT || 3002
+const SERVER_PORT = process.env.PORT || 3002
 
-console.log(SERVER_PORT);
-
-// db.sync({ force: false }).then((data) => {
-//   app.listen(SERVER_PORT, (req, res) => {
-//     console.log("Server Listening on port: " + SERVER_PORT);
-//   });
-// });
-
-app.listen(SERVER_PORT, (req, res) => {
-  console.log(`Server running in port ${SERVER_PORT}`);
+db.sync({ force: false }).then((data) => {
+  app.listen(SERVER_PORT, (req, res) => {
+    console.log("Server Listening on port: " + SERVER_PORT);
+  });
 });
