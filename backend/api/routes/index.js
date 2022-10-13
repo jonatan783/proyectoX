4d1862969b4d77c0b5bfe321af158ceeec2859dd
fede8932 <federi.angel@gmail.com>
2022-10-13 19:04:40 -0300

    actualizacion ruteo

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const admin = require("./admin/index");
-const auth = require('./auth/index');
+// const auth = require('./auth/index');
 // const compartidas = require('./compartidas/index');
 // const comprador = require('./comprador/index');
 // const superAdmin = require('./superAdmin/index');
 const vendedor = require("./vendedor/index");
 
 router.use("/admin", admin);
-router.use("/auth", auth);
+// router.use("/auth", auth);
 // router.use("/var", compartidas);
 // router.use("/comprador", comprador);
 // router.use("/superAdmin", superAdmin);
---
+++ backend/api/services/CategoryServices.js
@@ -0,0 +1,69 @@
+const { Product, Category } = require("../models");
+const { Op } = require("sequelize");
+
+class CategoryServices {
+  static async newCategory(req, next) {
+    const { name, description } = req.body;
+    try {
+      await Category.findOrCreate({
+        where: {
+          name
+        },
+        defaults: {
+          description
+        }
+      });
+      return "Categoría creada";
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async getAll(req, next) {
+    try {
+      const categorias = await Category.findAll();
+      return categorias;
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async getCatById(req, next) {
+    const { id } = req.params
+    try {
+      const categoria = await Category.findOne({
+        where: {
+          id: id
+        }
+      });
+      return categoria;
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async updateCatById(req, next) {
+    const { id } = req.params;
+    try {
+      await Category.update(req.body, {
+        where: {
+          id
+        }
+      });
+      return "Categoría actualizada";
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async deleteCategory(req, next) {
+    const { id } = req.params
+    try {
+      await Category.destroy({
+        where: {
+          id: id
+        }
+      });
+      return "Categoría eliminada";
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+module.exports = CategoryServices;
