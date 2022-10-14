f64109c22de444aeff23ee62f039d86579e2bda3
Alejandro Rusinoff <alejandrorusinoff@gmail.com>
2022-10-14 09:51:46 -0300

    agregue logeo por jwt v0.0.4

---
+++ backend/api/middlewares/jwt.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const checkJWT = (req, res, next) => {
+  if (!req.headers.authorization) return res.status(403).send("No posee los permisos necesarios para el contenido");
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  const data = jwt.verify(token, "albondiga");
+
+  if (data) {
+    req.user = data;
+    next();
+  } 
+  else {
+    return res.status(401).send("No tienen acceso aquí");
+  }
+};
+
+module.exports = checkJWT;
+
