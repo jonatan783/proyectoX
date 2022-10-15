54a8d9ddf3d7008a59a0e7a75fb41c6bb77f3c21
fede8932 <federi.angel@gmail.com>
2022-10-15 18:21:50 -0300

    alta container directus

--- backend/api/db/index.js
+++ backend/api/db/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+const db = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
