b6d987a21a55c4701ca52e09770b919b45d0d85e
fede8932 <federi.angel@gmail.com>
2022-10-15 19:38:14 -0300

    arreglando

--- backend/api/server.js
+++ backend/api/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use("/api", routes);
 
-const SERVER_PORT = process.env.PORT || 3002
+const SERVER_PORT = process.env.API_PORT || 3002
 
 db.sync({alter: true}).then((data) => {
   app.listen(SERVER_PORT, (req, res) => {
