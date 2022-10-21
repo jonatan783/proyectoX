9d48f9771ea6ff6503efb5d58001721c93286106
fede8932 <federi.angel@gmail.com>
2022-10-21 20:47:37 -0300

    reorganizacion de carpetas

--- backend/centralAPI/api/routes/comprador/productComment.js
+++ backend/centralAPI/api/routes/comprador/productComment.js
@@ -4,6 +4,6 @@ const commentController = require('../../controllers/commentController');
 const { isAuth } = require('../../middlewares/jwt');
 
 router.post('/add', isAuth,commentController.addComment); // Postear comentario
-router.get('/:productId', isAuth,commentController.getAllComments); // Ver todos los comentarios
+router.get('/:productId', commentController.getAllComments); // Ver todos los comentarios
 
 module.exports=router;
