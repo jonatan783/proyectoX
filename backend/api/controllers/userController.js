e996f811750cdbffe8d151e3e3fd6cd8ccf0793f
fede8932 <federi.angel@gmail.com>
2022-10-21 14:11:11 -0300

    act

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -3,7 +3,7 @@ const UserServices = require('../services/UserServices');
 class UserController {
   static async register(req, res, next) {
     try {
-      const respuesta = await UserServices.register(req, next);
+      const respuesta = await UserServices.register(req, res, next);
       return res.status(200).json(respuesta);
     } catch (err) {
       res.status(500).json(err);
@@ -11,7 +11,7 @@ class UserController {
   }
   static async login(req, res, next) {
     try {
-      const token = await UserServices.register(req, next);
+      const token = await UserServices.login(req, res, next);
       return res.status(200).json(token);
     } catch (err) {
       res.status(500).json(err);
@@ -19,7 +19,7 @@ class UserController {
   }
   static async userUpdate(req, res, next) {
     try {
-      const user = await UserServices.userUpdate(req, next);
+      const user = await UserServices.userUpdate(req, res, next);
       return res.status(200).json(user);
     } catch (err) {
       res.status(500).json(err);
