fdb5e8eee6a7aa07d2d3539982e65548ca81aea9
fede8932 <federi.angel@gmail.com>
2022-11-08 18:10:26 -0300

    act2

--- backend/centralAPI/api/routes/admin/category.js
+++ backend/centralAPI/api/routes/admin/category.js
@@ -5,7 +5,7 @@ const { isAuth, isAdmin } = require("../../middlewares/jwt");
 
 router.post("/new", isAdmin, category.newCategory); // Alta de categoria
 router.get("/getAll", isAuth, category.getAll); // Listar categorías
-router.get("/:id", isAuth, isAdmin, category.getCatById); // Buscar categoría por id
+router.get("/:id", isAuth, category.getCatById); // Buscar categoría por id
 router.put('/:id', isAuth, isAdmin, category.updateCatById); // Actualizar categoría
 router.delete("/:id", isAuth, isAdmin, category.deleteCategory); //Eliminar categoría por id
 
